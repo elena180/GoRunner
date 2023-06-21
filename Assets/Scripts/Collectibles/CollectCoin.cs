703a735b3b656900a398f365f865dc2b0209fbbc
elena <elena.angeleskafax@gmail.com>
2023-06-21 13:53:14 +0200

    Project files

---
+++ Assets/Scripts/Collectibles/CollectCoin.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectCoin : MonoBehaviour
+{
+    public AudioSource coinFX;    
+
+    void OnTriggerEnter(Collider other)
+    {
+        coinFX.Play();
+        CollectibleControl.coinCount += 1;
+        this.gameObject.SetActive(false);
+    }
+}
